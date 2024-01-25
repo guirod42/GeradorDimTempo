c842fb94779998105e1900cb5ea281be0c80921f
guilhermerodrigues <guilhermerodrigues@FCA-TI13>
2024-01-25 13:05:38 -0300

    FirstCommit

---
+++ DimTempoGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+
+
+namespace DimTempoGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
